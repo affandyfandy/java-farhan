ce2bfb0be7582ff16f583967d0ce9614b71100a0
AliRamdhan <aliramdhan4551@gmail.com>
2024-06-20 00:04:27 +0700

    update complete assignment

--- Week2-(18-21 June)/Assignment5/Q6.java
+++ Week2-(18-21 June)/Assignment5/Q6.java
@@ -1,30 +1,90 @@
 package Assignment5;
+/*
+ * https://medium.com/@reetesh043/the-copyonwritearraylist-internals-a-deep-dive-ff3ebad87697#:~:text=CopyOnWriteArrayList%20provides%20a%20thread%2Dsafe,consistency%20and%20isolation%20for%20readers.
+ */
 
+// public class Q6 {
+//     public static void main(String[] args) {
+//         // creating a CopyOnWriteArrayList
+//         CopyOnWriteArrayList<Integer> arrayList = new CopyOnWriteArrayList<>();
+
+//         // Adding elements to the list
+//         arrayList.add(1);
+//         arrayList.add(2);
+//         arrayList.add(3);
+//         System.out.println("Size of arrayList after additions: " + arrayList.size());
+
+//         // Display the contents of the list
+//         System.out.println("Contents of arrayList: " + arrayList);
+
+//         // Iterate over the list and modify it
+//         for (Integer number : arrayList) {
+//             System.out.println("Current Element: " + number);
+//             arrayList.add(number + 5); // Modify the list by adding 10 to each element
+//         }
+//         // add new data into jago
+//         arrayList.add(40);
+
+//         // Print modified list
+//         System.out.println("Modified List: " + arrayList);
+//     }
+// }
+import java.util.ArrayList;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Q6 {
     public static void main(String[] args) {
-        // creating a CopyOnWriteArrayList
-        CopyOnWriteArrayList<Integer> arrayList = new CopyOnWriteArrayList<>();
-
-        // Adding elements to the list
+        // ArrayList Example
+        List<Integer> arrayList = new ArrayList<>();
         arrayList.add(1);
         arrayList.add(2);
         arrayList.add(3);
-        System.out.println("Size of arrayList after additions: " + arrayList.size());
 
-        // Display the contents of the list
-        System.out.println("Contents of arrayList: " + arrayList);
-
-        // Iterate over the list and modify it
-        for (Integer number : arrayList) {
-            System.out.println("Current Element: " + number);
-            arrayList.add(number + 5); // Modify the list by adding 10 to each element
+        try {
+            // Iterating and modifying an ArrayList using an Iterator
+            Iterator<Integer> arrayListIterator = arrayList.iterator();
+            while (arrayListIterator.hasNext()) {
+                Integer num = arrayListIterator.next();
+                if (num == 2) {
+                    // This will throw a ConcurrentModificationException in ArrayList
+                    arrayList.add(4); // Concurrent modification exception!                
+                }
+            }
+        } catch (ConcurrentModificationException e) {
+            System.out.println("Caught ConcurrentModificationException in ArrayList!");
         }
-        // add new data into jago
-        arrayList.add(40);
 
-        // Print modified list
-        System.out.println("Modified List: " + arrayList);
+        // List all data in arrayList after the attempted modification
+        System.out.println("Contents of arrayList after modification attempt:");
+        for (Integer num : arrayList) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+        // CopyOnWriteArrayList Example
+        List<Integer> copyOnWriteArrayList = new CopyOnWriteArrayList<>();
+        copyOnWriteArrayList.add(1);
+        copyOnWriteArrayList.add(2);
+        copyOnWriteArrayList.add(3);
+
+        // Iterating and modifying a CopyOnWriteArrayList using an Iterator
+        Iterator<Integer> copyOnWriteArrayListIterator = copyOnWriteArrayList.iterator();
+        while (copyOnWriteArrayListIterator.hasNext()) {
+            Integer num = copyOnWriteArrayListIterator.next();
+            if (num == 2) {
+                // This modification is allowed in CopyOnWriteArrayList
+                copyOnWriteArrayList.add(4); // No ConcurrentModificationException!
+            }
+        }
+
+        // List all data in copyOnWriteArrayList after the modification
+        System.out.println("Contents of copyOnWriteArrayList after modification:");
+        for (Integer num : copyOnWriteArrayList) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
     }
 }
--- Week2-(18-21 June)/Assignment5/Q7.java
+++ Week2-(18-21 June)/Assignment5/Q7.java
@@ -1,5 +1,53 @@
 package Assignment5;
 
+/*
+ * ConcurrentHashMap: It allows concurrent access to the map.
+ * ConcurrentHashMap is a thread-safe implementation of the Map interface in Java, which means multiple threads can access it simultaneously without any synchronization issues
+ * https://dzone.com/articles/how-concurrenthashmap-works-internally-in-java
+ */
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Q7 {
-    
+    public static void main(String[] args) {
+        // Create a ConcurrentHashMap
+        ConcurrentHashMap<String, Integer> map = new ConcurrentHashMap<>();
+
+        // Create an ExecutorService to manage threads
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        // Runnable task that increments the count for a key
+        Runnable task1 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                map.merge("key1", 1, Integer::sum);
+            }
+        };
+
+        // Runnable task that increments the count for another key
+        Runnable task2 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                map.merge("key2", 1, Integer::sum);
+            }
+        };
+
+        // Submit tasks to the executor
+        executor.submit(task1);
+        executor.submit(task2);
+        executor.submit(task1); // Submitting task1 again to test concurrency
+        
+        // Shutdown the executor
+        executor.shutdown();
+
+        try {
+            // Wait for all tasks to finish
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Print the map contents
+        System.out.println("ConcurrentHashMap content: " + map);
+    }
 }
