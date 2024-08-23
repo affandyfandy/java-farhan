ae1e40fb88602f56cdc12c09fc032ea8b6ec85e1
AliRamadhan <aliramdhan4551@gmail.com>
2024-08-23 14:50:05 +0700

    feat : add date pipe

--- Week 11/AngularStandAlone/src/app/modules/customers/components/customers/customers.component.ts
+++ Week 11/AngularStandAlone/src/app/modules/customers/components/customers/customers.component.ts
@@ -1,11 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, DatePipe],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css',
 })
-export class CustomersComponent {}
+export class CustomersComponent {
+  currentDate: Date = new Date();
+}
