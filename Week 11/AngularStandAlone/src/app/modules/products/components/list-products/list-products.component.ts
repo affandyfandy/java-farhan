433a6a2aad3e12ba128ba20729331fca692333b8
AliRamadhan <aliramdhan4551@gmail.com>
2024-08-23 16:02:37 +0700

    feat : add search by name in list products table

--- Week 11/AngularStandAlone/src/app/modules/products/components/list-products/list-products.component.ts
+++ Week 11/AngularStandAlone/src/app/modules/products/components/list-products/list-products.component.ts
@@ -4,18 +4,29 @@ import { ColDef } from 'ag-grid-community';
 import { ProductService } from '../../../../service/product.service';
 import { Product } from '../../../../core/interfaces/product.type';
 import { DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-list-products',
   standalone: true,
-  imports: [AgGridAngular, DatePipe],
+  imports: [AgGridAngular, DatePipe, FormsModule],
   templateUrl: './list-products.component.html',
   styleUrls: ['./list-products.component.css'],
-  providers: [DatePipe], // Add DatePipe here
+  providers: [DatePipe],
 })
 export class ListProductsComponent implements OnInit {
   products: Product[] = [];
   rowData: Product[] = [];
+  productName = '';
+
+  colDefs: ColDef[] = [
+    { field: 'name', headerName: 'Product Name' },
+    { field: 'price', headerName: 'Price' },
+    { field: 'status', headerName: 'Status' },
+    { field: 'quantity', headerName: 'Quantity' },
+    { field: 'createdAt', headerName: 'Created At' },
+    { field: 'updatedAt', headerName: 'Updated At' },
+  ];
 
   constructor(
     private productService: ProductService,
@@ -29,23 +40,33 @@ export class ListProductsComponent implements OnInit {
   loadProducts(): void {
     this.productService.getAllProducts().subscribe((data) => {
       this.products = data;
-      this.rowData = this.products.map((product) => ({
-        name: product.name,
-        price: product.price,
-        status: product.status,
-        quantity: product.quantity,
-        createdAt: this.datePipe.transform(product.createdAt, 'fullDate'),
-        updatedAt: this.datePipe.transform(product.updatedAt, 'fullDate'),
-      }));
+      this.updateRowData(this.products);
     });
   }
 
-  colDefs: ColDef[] = [
-    { field: 'name', headerName: 'Name' },
-    { field: 'price', headerName: 'Price' },
-    { field: 'status', headerName: 'Status' },
-    { field: 'quantity', headerName: 'Quantity' },
-    { field: 'createdAt', headerName: 'Created At' },
-    { field: 'updatedAt', headerName: 'Updated At' },
-  ];
+  onSearch(): void {
+    this.searchProductName();
+  }
+
+  searchProductName(): void {
+    this.productService.findByName(this.productName).subscribe({
+      next: (data) => {
+        this.products = data;
+        this.updateRowData(this.products);
+        console.log('data', data);
+      },
+      error: (e) => console.error(e),
+    });
+  }
+
+  updateRowData(products: Product[]): void {
+    this.rowData = products.map((product) => ({
+      name: product.name,
+      price: product.price,
+      status: product.status,
+      quantity: product.quantity,
+      createdAt: this.datePipe.transform(product.createdAt, 'fullDate'),
+      updatedAt: this.datePipe.transform(product.updatedAt, 'fullDate'),
+    }));
+  }
 }
